1a29446384b28afd63e5f99abdaa24f85f283b81
shota kikuchi <shota3557@gmail.com>
2022-12-05 23:00:05 +0900

    アソシエーションメソッドを定義した

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   mount_uploader :picture, TaskPictureUploader
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   has_many :causes, dependent: :destroy
 end
