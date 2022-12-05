51fd2fd824a7db6c87cf5633bd8c81ec8258c8ff
shota kikuchi <shota3557@gmail.com>
2022-12-05 11:44:09 +0900

    task_paramsメソッドを定義した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,25 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task= current_user.tasks.build(task_params) 
+    if @task.save
+      redirect_to new_cause_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :picture, :image_cache)
   end
 end
+
