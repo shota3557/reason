35a1e1201165ff2bb963413fcfb689e1da792fd7
shota kikuchi <shota3557@gmail.com>
2022-12-05 11:42:54 +0900

    パラメータを設定した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation, :admin ]
+    added_attrs = [ :name, :email, :password, :password_confirmation, :admin, :user_id ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
