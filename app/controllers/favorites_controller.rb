3d7257feeb90feab25567b16149e4e35b937502d
shota kikuchi <shota3557@gmail.com>
2022-12-06 22:54:35 +0900

    destroyアクションを追加した

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,4 +2,14 @@ class FavoritesController < ApplicationController
   def index
     @favorites = Favorite.all
   end
+    
+  def create
+    favorite = current_user.favorites.create(task_id: params[:task_id])
+    redirect_to tasks_path, notice: "登録した"
+  end
+  
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id]).destroy
+    redirect_to tasks_path, notice: "解除した"
+  end
 end
