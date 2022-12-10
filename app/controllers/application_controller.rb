0045ffe71bb9ffa9082a7c8d979a8c0e96ae6dad
shota kikuchi <shota3557@gmail.com>
2022-12-10 17:53:37 +0900

    before_action :authenticate_userを追加した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protected
 
   def configure_permitted_parameters
-    added_attrs = [ :name, :email, :password, :password_confirmation, :admin, :user_id ]
+    added_attrs = [ :name, :email, :picture, :password, :password_confirmation, :admin, :user_id ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
