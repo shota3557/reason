f2880240f0be03835cc3c1bf14bd7f141a888f58
shota kikuchi <shota3557@gmail.com>
2022-12-10 18:44:29 +0900

    self.guestメソッドを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_tasks, through: :tasks, source: :task
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
