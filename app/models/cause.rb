92a0bcad1f52545d89d77bfd28da6c718c52eb30
shota kikuchi <shota3557@gmail.com>
2022-12-10 11:48:07 +0900

    4行目にdependent: :destroyを追加した

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,7 +1,7 @@
 class Cause < ApplicationRecord
   mount_uploader :picture, CausePictureUploader
   mount_uploader :movie, CauseMovieUploader
-  has_many :solutions
+  has_many :solutions, dependent: :destroy
   accepts_nested_attributes_for :solutions, reject_if: :all_blank, allow_destroy: true
   belongs_to :task
 end
