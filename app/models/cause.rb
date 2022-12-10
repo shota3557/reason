baa315ae70d851ac9b53cb26bbac47eaa7e76f22
shota kikuchi <shota3557@gmail.com>
2022-12-10 11:39:14 +0900

    accepts_nested_attributes_for :solutionsの位置を変更した

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,7 +1,7 @@
 class Cause < ApplicationRecord
   mount_uploader :picture, CausePictureUploader
   mount_uploader :movie, CauseMovieUploader
-  accepts_nested_attributes_for :solutions, reject_if: :all_blank, allow_destroy: true
   has_many :solutions
+  accepts_nested_attributes_for :solutions, reject_if: :all_blank, allow_destroy: true
   belongs_to :task
 end
