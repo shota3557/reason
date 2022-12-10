403db2e85802c4019496b975f0975a47a7492b1d
shota kikuchi <shota3557@gmail.com>
2022-12-10 17:55:12 +0900

    registrations: 'users/registrations'に変更した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: registrations: 'users/registrations'
   }
   root 'tasks#index'
   resources :tasks, except: [:show] do
