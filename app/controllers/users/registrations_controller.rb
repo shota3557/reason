6de2c17d11fb4956035bc82567256b4547f8de5a
shota kikuchi <shota3557@gmail.com>
2022-12-11 16:21:42 +0900

    ensure_admin_userメソッドを追加した

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,16 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: %i[update destroy]
+  before_action :ensure_admin_user, only: %i[update destroy]
+
   def ensure_normal_user
-    if resource.email == 'guest@example.com'
+    if params[:user][:email].downcase == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーの更新・削除はできません。'
+    end
+  end
+
+  def ensure_admin_user
+    if params[:user][:email].downcase == 'admin_guest@guest.com'
+      redirect_to root_path, alert: 'ゲスト管理者は削除できません'
     end
   end
 
