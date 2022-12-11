d084c85de09b4069d749d45228ef0ec9a7c9638e
shota kikuchi <shota3557@gmail.com>
2022-12-11 16:25:18 +0900

    post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    post 'users/admin_guest_sign_in', to: 'users/sessions#admin_guest_sign_in'
   end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
