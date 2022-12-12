b5435fd67e9e58d973b0682860e01d5212e69498
shota kikuchi <shota3557@gmail.com>
2022-12-12 16:59:35 +0900

    choose_new_or_editメソッドを追記した

--- app/helpers/causes_helper.rb
+++ app/helpers/causes_helper.rb
@@ -1,2 +1,9 @@
 module CausesHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      task_causes_path
+    elsif action_name == 'edit'
+      task_cause_path method: :patch
+    end
+  end
 end
