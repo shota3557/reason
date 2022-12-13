87fcdd55fdd3e23aad6cedbd01f5da9d3e8c411d
shota kikuchi <shota3557@gmail.com>
2022-12-13 16:11:58 +0900

    修正した

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :causes, dependent: :destroy
-  accepts_nested_attributes_for :causes, reject_if: :all_blank, allow_destroy: true
   has_many :favorite_users, through: :favorites, source: :user
+  accepts_nested_attributes_for :causes, reject_if: :all_blank, allow_destroy: true
   validates :name, presence: true
   validates_associated :causes
 end
