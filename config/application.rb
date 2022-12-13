1e956acf3e7a48727265e70b6d9c8fcbf08a2d2b
shota kikuchi <shota3557@gmail.com>
2022-12-13 16:09:10 +0900

    コメントアウトを削除した

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,5 @@ module Reason
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :ja
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
