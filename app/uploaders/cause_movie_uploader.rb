f592d2b8fb8fbaef3694f1ec56ce664ded1db9c2
shota kikuchi <shota3557@gmail.com>
2022-12-14 10:49:38 +0900

    storage :fogに変更した

--- app/uploaders/cause_movie_uploader.rb
+++ app/uploaders/cause_movie_uploader.rb
@@ -4,7 +4,7 @@ class CauseMovieUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
