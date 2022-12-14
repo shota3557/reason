5295ba48da43ee27f43d5732ab8da388201d60ac
shota kikuchi <shota3557@gmail.com>
2022-12-14 16:39:15 +0900

    テスト環境のバケットを追加した

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,5 +23,8 @@ CarrierWave.configure do |config|
       when 'development'
         config.fog_directory = 'reason2-development'
         config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/reason2-development'
+      when 'test'
+        config.fog_directory = 'reason2-test'
+        config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/reason2-test'  
     end
   end
