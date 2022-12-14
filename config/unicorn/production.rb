46d4bb5f9bc41fec456ffa027d22c281ceee8236
shota kikuchi <shota3557@gmail.com>
2022-12-14 16:53:05 +0900

    app_dirを変更した

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,7 @@
 $worker  = 2
 $timeout = 30
 #自分のアプリケーション名（currentがつくことに注意）
-$app_dir = "/var/www/cause/current"
+$app_dir = "/var/www/reason/current"
 $listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
