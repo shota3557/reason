189ff18ed004a408faa76ec07d339dc058549ddf
shota kikuchi <shota3557@gmail.com>
2022-12-14 13:49:43 +0900

    production.rbの記述を変更した

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,20 +2,20 @@ $worker  = 2
 $timeout = 30
 #自分のアプリケーション名（currentがつくことに注意）
 $app_dir = "/var/www/cause/current"
-$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $cause_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $cause_dir
-$std_log = File.expand_path 'log/unicorn.log', $cause_dir
+$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'log/unicorn.log', $app_dir
 
 # 上記で設定したものが適応されるよう定義
 worker_processes  $worker
-working_directory $cause_dir
+working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout
 listen  $listen
 pid $pid
 
-preload_cause true
+preload_app true
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
