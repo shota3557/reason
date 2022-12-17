31e44511812989a11d8b3ccf76d61052dfd640ea
shota kikuchi <shota3557@gmail.com>
2022-12-17 17:21:42 +0900

    seedデータを作成した

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+User.create!(
+  name: "user#{n+1}",
+  email: "user#{n+1}@example.com",
+  password: "password" )
+end
+
+5.times do |n|
+  User.all.each do |user|
+    Task.create!(user_id: user.id, name: "title#{n+1}", 
+    causes_attributes: [content: "cause#{n+1}",
+    solutions_attributes: [content: "solution#{n+1}"]])
+  end
+end  
