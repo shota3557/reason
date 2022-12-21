1787a8012863d39609a7301fd46e5c26d1bdc4e0
shota kikuchi <shota3557@gmail.com>
2022-12-21 17:13:24 +0900

    不要な半角スペースを削除した

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -2,7 +2,7 @@ class Cause < ApplicationRecord
   mount_uploader :picture, CausePictureUploader
   mount_uploader :movie, CauseMovieUploader
   has_many :solutions, dependent: :destroy
-  accepts_nested_attributes_for :solutions,  allow_destroy: true
+  accepts_nested_attributes_for :solutions, allow_destroy: true
   belongs_to :task
   validates :content, presence: true
 end
